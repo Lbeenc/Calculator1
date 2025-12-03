e8ed6b7fdf3634cfe3199449d1ac96cc9eee1ed6
Lbeenc <Lbeenc@gmail.com>
2025-12-02 18:32:43 -0600

    Update main.py
    Added pow and root ability
    added history function
    added a save function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+#for allowing decimal points
 from decimal import Decimal
 
-
+last_output = []
 output_history = []
+clean_history= []
+Output = [0]
 
 
 
-
-
+#basic ui functions
 def ui():
     com = input('what would you like to do?').lower()
     if com == "add":
@@ -17,8 +19,20 @@ def ui():
         mul()
     elif com == "divide":
         div()
+    elif com == "power":
+        pow()
+    elif com == "root":
+        root()
+
     elif com == "history":
+        phrase = 'Decimal'
         print(output_history)
+        clean_history = (str(output_history).replace(phrase,""))
+        print(clean_history)
+        print('last output:', Output[0])
+
+    elif com == "save":
+        save()
     elif com == "exit":
         return
     else:
@@ -26,13 +40,15 @@ def ui():
     ui()
 
 
-
+#math functions
 def add():
     x = input('enter first number')
     y = input('enter second number')
     try:
-        print(Decimal(x) + Decimal(y))
-        output_history.append(Decimal(x) + Decimal(y))
+        output =  Decimal(x) + Decimal(y)
+        print(output)
+        output_history.append(output)
+        Output[0] = output
     except:
         print('that is not a valid number')
 
@@ -41,8 +57,10 @@ def sub():
     x = input('enter first number')
     y = input('enter second number')
     try:
-        print(Decimal(x) - Decimal(y))
-        output_history.append(Decimal(x) - Decimal(y))
+        output = Decimal(x) - Decimal(y)
+        print(output)
+        output_history.append(output)
+        Output[0] = output
 
     except:
         print('that is not a valid number')
@@ -51,18 +69,52 @@ def mul():
     x = input('enter first number')
     y = input('enter second number')
     try:
-        print(Decimal(x) * Decimal(y))
-        output_history.append(Decimal(x) * Decimal(y))
+        output = Decimal(x) * Decimal(y)
+        print(output)
+        output_history.append(output)
+        Output[0] = output
     except:
         print('that is not a valid number')
 def div():
     x = input('enter first number')
     y = input('enter second number')
     try:
-        print(Decimal(x) /Decimal(y))
-        output_history.append(Decimal(x) / Decimal(y))
+        output = Decimal(x) /Decimal(y)
+        print(output)
+        output_history.append(output)
+        Output[0] = output
     except:
         print('that is not a valid number')
+
+def pow():
+    x = input('enter first number')
+    y = input('enter second number (exponent)')
+    try:
+        output = Decimal(x) ** Decimal(y)
+        print(output)
+        output_history.append(output)
+        Output[0] = output
+    except:
+        print('that is not a valid number')
+
+def root():
+    x = input('enter number to find the square root of')
+    try:
+        output = Decimal(x) ** 0.5
+        print(output)
+        Output[0] = output
+
+    except:
+        print('that is not a valid number')
+
+#saving to log file function
+def save():
+    print('saved output history to file')
+    with open('calc_output.txt', 'a') as f:
+        f.write('log' + str(output_history) + '\n')
+
+
+
 print('Welcome to Calculator\n')
 
 print('you can add subtract , multiply, and divide.\n')
@@ -70,7 +122,11 @@ print('enter "Add" to add numbers.')
 print('enter "Subtract" to subtract numbers.')
 print('enter "Multiply" to multiply numbers.')
 print('enter "Divide" to divide numbers.')
+print('enter "Power" to raise to the power of numbers.')
+print('enter "Root" to take the root of the number.')
 print('enter "History" to show history.')
+print('enter "Save" to save output history.')
 print('enter "Exit" to exit the program.')
 
 ui()
+
